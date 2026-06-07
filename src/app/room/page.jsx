0a7fa04a3be7123c6d8b7d0d79b02287efdfcdf7
raselmondol2344 <raselmondream245@gmail.com>
2026-06-07 10:49:58 +0600

    filter section added

--- src/app/room/page.jsx
+++ src/app/room/page.jsx
@@ -9,7 +9,7 @@ const allroomspage = async() => {
     //console.log(roomsdata);
     return (
 
-        
+ 
         <div >
             <h1 className="font-bold text-xl p-10 flex justify-start ">All Room</h1>
             <RoomList initialRooms={roomsdata}></RoomList>
